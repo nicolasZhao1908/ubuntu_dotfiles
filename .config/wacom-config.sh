29064c274588b4de36b262e09503d2cf533005b7
nicolasZhao1908 <0619971117@uma.es>
2023-05-29 10:37:04 +0200

    feat: add wacom udev config

---
+++ .config/wacom-config.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+for i in $(seq 10); do
+    if xsetwacom list devices | grep -q Wacom; then
+        break
+    fi
+    sleep 1
+done
+
+list=$(xsetwacom list devices)
+pad=$(echo "${list}" | awk '/pad/{print $7}')
+stylus=$(echo "${list}" | xsetwacom list devices | awk '/stylus/{print $7}')
+
+if [ -z "${pad}" ]; then
+    exit 0
+fi
+
+xsetwacom set ${stylus} MapToOutput eDP
+xsetwacom set ${stylus} Button 2 "key p"
+xsetwacom set ${stylus} Button 3 "key e"
+xsetwacom set ${pad} Button 1 "key +ctrl z -ctrl"
+xsetwacom set ${pad} Button 2 "key +ctrl y -ctrl"
+xsetwacom set ${pad} Button 3 "key +super 1 -super"
+xsetwacom set ${pad} Button 8 "key +super 3 -super"
--- utils/lib.sh
+++
@@ -1,3 +0,0 @@
-pdf2png(){
-    gs -sDEVICE=pngalpha -o static-%03d.png -r144 $1
-}
